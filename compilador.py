b8525a6e774eb0a6c841366c50c3dac42a450106
Ana Bento <95715418+anabent0@users.noreply.github.com>
2025-02-06 11:53:15 -0300

    Adição de expressoes

--- compilador.py
+++ compilador.py
@@ -3,9 +3,8 @@ import ply.yacc as yacc
 from dicionario import DICIONARIO
 
 # Definição dos tokens
-tokens = ('PRONOME', 'VERBO', 'SUBSTANTIVO', 'PONTUACAO')
+tokens = ('PRONOME', 'VERBO', 'SUBSTANTIVO', 'EXPRESSOES')
 
-# Expressões regulares para cada categoria gramatical
 def t_PRONOME(t):
     r'我|你|他|她|我们|你们|他们'
     return t
@@ -18,7 +17,9 @@ def t_SUBSTANTIVO(t):
     r'米饭|茶|咖啡|饺子|牛肉面|果汁|蛋糕|学校|电影|书|朋友|老师|学生|饭馆|桌子|椅子'
     return t
 
-t_PONTUACAO = r'[。，？！]'
+def t_EXPRESSOES(t):
+    r'谢谢|不客气|请|啊不|对不起|没关系|嗨|再见|加油|好|坏|当然|真的吗|没问题|对吗'
+    return t
 
 t_ignore = ' \t'
 
@@ -28,11 +29,17 @@ def t_error(t):
 
 lexer = lex.lex()
 
-# Regras gramaticais separadas em categorias
 def p_frase(p):
-    '''frase : pronome verbo substantivo PONTUACAO
-             | pronome verbo substantivo'''
-    p[0] = p[1:]  # Mantém a estrutura sintática
+    '''frase : pronome verbo substantivo
+             | pronome verbo substantivo expressao
+             | expressao pronome verbo substantivo
+             | expressao pronome verbo substantivo expressao'''
+    if len(p) == 4:
+        p[0] = [p[1], p[2], p[3]]
+    elif len(p) == 5:
+        p[0] = [p[1], p[2], p[3], p[4]]
+    else:
+        p[0] = [p[1], p[2], p[3], p[4], p[5]]
 
 def p_pronome(p):
     'pronome : PRONOME'
@@ -45,6 +52,10 @@ def p_verbo(p):
 def p_substantivo(p):
     'substantivo : SUBSTANTIVO'
     p[0] = p[1]
+    
+def p_expressao(p):
+    'expressao : EXPRESSOES'
+    p[0] = p[1]
 
 def p_error(p):
     if p:
@@ -54,7 +65,6 @@ def p_error(p):
 
 parser = yacc.yacc()
 
-# Funções de tradução separadas
 def traduzir_hanzi(frase):
     tokens = parser.parse(frase)
     return " ".join(tokens) if tokens else ""
--- dicionario.py
+++ dicionario.py
@@ -39,9 +39,20 @@ DICIONARIO = {
     "桌子": ("zhuōzi", "mesa"),
     "椅子": ("yǐzi", "cadeira"),
 
-    # Pontuação
-    "。": (".", "."),
-    "，": (",", ","),
-    "？": ("?", "?"),
-    "！": ("!", "!")
+    # Expressões
+    "谢谢": ("xièxiè", "obrigado"),
+    "不客气": ("bú kèqì", "de nada"),
+    "请": ("qǐng", "por favor"),
+    "啊不": ("ā bù", "ah não"),
+    "对不起": ("duìbuqǐ", "desculpe"),
+    "没关系": ("méi guānxi", "não tem problema"),
+    "嗨": ("hāi", "olá"),
+    "再见": ("zàijiàn", "tchau"),
+    "加油": ("jiāyóu", "força!"),
+    "好": ("hǎo", "bom"),
+    "坏": ("huài", "ruim"),
+    "对吗": ("duì ma", "certo?"),
+    "当然": ("dāngrán", "claro"),
+    "真的吗": ("zhēn de ma", "sério?"),
+    "没问题": ("méi wèntí", "sem problema")
 }
